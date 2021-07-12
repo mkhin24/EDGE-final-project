76c37049cb876ed3fca0961dc1efeb84e02dff88
mkhin24 <mtk16.may@precollegiateyangon.org>
2021-07-12 23:23:07 +0630

    Update choice game.py
    First question done. Looking for ways to shorten code.

--- choice game.py
+++ choice game.py
@@ -5,7 +5,13 @@ print("Mingalarbar !")
 choice = input("Would you like to play a game? yes/no: ")
 
 if choice == "yes":
-    print("Okiee")
+    print("")
+    print("Type in a number to continue.\n")
+    answer1 = input("The aliens have invaded.\nThey have promised to honor the alien code and grant you a wish if you tell them your bestfriend's secret. Would you do it?\n(1)Of course,not!\n(2)Sorry,bestie. It was too tempting.\n")
+    if answer1 == "1":
+        print("okay")
+    elif answer1 == "2":
+        print("okay up to here")
 elif choice == "no":
     print("That's too bad, but have a great day!!")
 else:
