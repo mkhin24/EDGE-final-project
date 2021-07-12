b3a55cf3a21c5049bf1ec9d9910a5037814e5075
mkhin24 <mtk16.may@precollegiateyangon.org>
2021-07-12 22:08:34 +0630

    Create choice game.py
    Asking user yes or not before the game starts.

---
+++ choice game.py
@@ -0,0 +1,12 @@
+#Mays_Game
+version = "v 1.0"
+
+print("Mingalarbar !")
+choice = input("Would you like to play a game? yes/no: ")
+
+if choice == "yes":
+    print("Okiee")
+elif choice == "no":
+    print("That's too bad, but have a great day!!")
+else:
+    print("(system error)\nPress (control/command + E) to restart.")
