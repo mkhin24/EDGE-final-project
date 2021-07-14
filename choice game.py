fc6bb7e1d1800dd36968e72ec0ab86dc605e7027
mkhin24 <mtk16.may@precollegiateyangon.org>
2021-07-14 14:41:12 +0630

    Update choice game.py
    did some debugging: saving before going further

--- choice game.py
+++ choice game.py
@@ -3,14 +3,15 @@ version = "v 1.0"
 
 question1 = "The aliens have invaded.\nThey have promised to honor the alien code and grant you a wish if you tell them your bestfriend's secret. Would you do it?\n(1)Of course,not!\n(2)Sorry,bestie. It was too tempting.\n"
 
-question21 = "Noble choice! Sadly,your bestie betrayed you! Now, you're on the run from the aliens. Will you:\n(1)Chase down your friend OR\n(2)Go into hiding and plan your next move ? "
+question21 = "Noble choice! Sadly,your bestie betrayed you! Now, you're on the run from the aliens. Will you:\n(1)Team up with the aliens to chase down your friend OR\n(2)Hide from them?\n "
 
-question22 = "Congrats! The aliens have given you fringe benefits. Yet, they threatened you to keep on leaking secrets in exchange for your life. What will you do?\n(1)Call them out for cheating and fight.\n(2)Agree hesitantly with hope for the future."
+question22 = "Congrats! The aliens have offered you a rank on one condition: you have to kill your bestfried. What will you do?\n(1)Kill your bestie\n(2)Agree hesitantly with hope for the future.\n"
 
-print("May's Game version 1.0\nMingalarbar !")
-choice = input("Would you like to play a game? yes/no: ")
+print("May's Game version 1.0\nMingalarbar !\n")
+x = input("Would you like to play a game? yes/no: ")
+x = x.strip()
 
-if choice == "yes" or "YES" or "Yes":
+if x == "yes":
     print("")
     print("Type in a number to continue.\n")
     answer1 = input(question1)
@@ -22,9 +23,10 @@ if choice == "yes" or "YES" or "Yes":
             print("okiee")
     elif answer1 == "2":
         answer22 = input(question22)
+        if answer22 == "yes":
+            print("okay too")
 
-elif choice == "no" or "NO" or "No":
+elif x == "no":
     print("That's too bad, but have a great day!!")
-
 else:
     print("(system error)\nPress (control/command + E) to restart.")
